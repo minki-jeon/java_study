4d707048d93d2d5b3d897d0c116955de4c7e8786
minki-jeon <minki.jeon.study@gmail.com>
2025-01-07 18:32:21 +0900

    불필요한 소스 삭제

--- src/main/study/java/parameterization/FilteringTest.java
+++ src/main/study/java/parameterization/FilteringTest.java
@@ -4,9 +4,7 @@ import static java.util.Arrays.asList;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Function;
 import java.util.function.Predicate;
-import java.util.stream.Collectors;
 
 import main.study.java.func.predicate.Tshirt;
 
@@ -23,12 +21,6 @@ public class FilteringTest {
 				new Tshirt(500, "white"));
 		Predicate<Tshirt> priceFilter = t -> t.getPrice() > 300;
 		List<Tshirt> filterColors = filter(myShirtList, priceFilter);
-//		Function<Tshirt, String> colorMapper = Tshirt::getColor;
-		
-//		List<String> filterColors = myShirtList.stream()
-//											.filter(priceFilter)
-//											.map(colorMapper)
-//											.collect(Collectors.toList());
 		System.out.println(filterColors);
 
 	}

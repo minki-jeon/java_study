a4e42dce95680b4b43204d00f5e4423c5b3c60c3
minki-jeon <minki.jeon.study@gmail.com>
2025-01-07 17:57:40 +0900

    Combination 응용테스트 (Tshirt 객체 활용)

--- src/main/study/java/parameterization/CombinationExample.java
+++ src/main/study/java/parameterization/CombinationExample.java
@@ -1,10 +1,14 @@
 package main.study.java.parameterization;
 
+import static java.util.Arrays.asList;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
+
+import main.study.java.func.predicate.Tshirt;
 
 public class CombinationExample {
 	//* https://github.com/haru-note/tech-interview/blob/main/src/main/java/net/harunote/java8/parameterization/CombinationExample.java
@@ -22,6 +26,25 @@ public class CombinationExample {
 												.map(lengthMapper)
 												.collect(Collectors.toList());
 		System.out.println(filteredWordLengths);
+		
+		//* Test
+		List<Tshirt> myShirtList = asList(new Tshirt(100, "yellow"),
+				new Tshirt(200, "red"),
+				new Tshirt(400, "purple"),
+				new Tshirt(600, "red"),
+				new Tshirt(200, "green"),
+				new Tshirt(300, "blue"),
+				new Tshirt(400, "red"),
+				new Tshirt(500, "white"));
+		Predicate<Tshirt> priceFilter = t -> t.getPrice() > 300;
+		Function<Tshirt, String> colorMapper = Tshirt::getColor;
+		
+		List<String> filterColors = myShirtList.stream()
+											.filter(priceFilter)
+											.map(colorMapper)
+											.collect(Collectors.toList());
+		System.out.println(filterColors);
+		
 	}
 
 }

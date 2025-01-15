7de3579e183bc3406e322c47d211b4b6126ceb4e
minki-jeon <minki.jeon.study@gmail.com>
2025-01-15 20:43:45 +0900

    객체에 당첨개수, 보너스일치여부 추가

--- src/main/study/practice/PersonLotto.java
+++ src/main/study/practice/PersonLotto.java
@@ -4,6 +4,8 @@ public class PersonLotto {
 	
 	private String name;
 	private int[] numbers;
+	private int winCount;
+	private boolean isBonus;
 	
 	public PersonLotto(String name, int[] numbers) {
 		this.name = name;
@@ -36,6 +38,34 @@ public class PersonLotto {
 	 */
 	public void setNumbers(int[] numbers) {
 		this.numbers = numbers;
+	}
+
+	/**
+	 * @return the winCount
+	 */
+	public int getWinCount() {
+		return winCount;
+	}
+
+	/**
+	 * @param winCount the winCount to set
+	 */
+	public void setWinCount(int winCount) {
+		this.winCount = winCount;
+	}
+
+	/**
+	 * @return the isBonus
+	 */
+	public boolean isBonus() {
+		return isBonus;
+	}
+
+	/**
+	 * @param isBonus the isBonus to set
+	 */
+	public void setBonus(boolean isBonus) {
+		this.isBonus = isBonus;
 	}
 	
 	

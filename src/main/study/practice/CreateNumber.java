55c9908ed170fc34ba718c3b4c4b251cde49346f
minki-jeon <minki.jeon.study@gmail.com>
2025-01-20 20:01:49 +0900

    번호 생성 조건값 저장 객체

---
+++ src/main/study/practice/CreateNumber.java
@@ -0,0 +1,27 @@
+package main.study.practice;
+
+public class CreateNumber {
+	
+	/**
+	 * 로또복권 기준
+	 * count = 6
+	 * max = 45
+	 */
+	
+	private static final int count = 5;		// 생성 숫자의 개수
+	private static final int max = 8;		// 생성 숫자의 최대값
+	
+	/**
+	 * @return the count
+	 */
+	public final int getCount() {
+		return count;
+	}
+	/**
+	 * @return the max
+	 */
+	public final int getMax() {
+		return max;
+	}
+	
+}

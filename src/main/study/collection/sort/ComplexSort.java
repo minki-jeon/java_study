0581d162df5e892d8629d3bda88eafd39c53b70d
minki-jeon <minki.jeon.study@gmail.com>
2025-02-03 20:14:18 +0900

    Working ComplexSort, Add logging (TODO Check WARNNING.)

--- src/main/study/collection/sort/ComplexSort.java
+++ src/main/study/collection/sort/ComplexSort.java
@@ -5,10 +5,14 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import org.apache.commons.collections4.comparators.ComparatorChain;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ComplexSort {
 	//* https://github.com/haru-note/tech-interview/blob/main/src/main/java/net/harunote/collection/sort/ComplexSort.java
 
+	//* Java Build Path -> Libraries -> Classpath -> Add External JARs -> "slf4j-api-1.7.30.jar", "slf4j-log4j12-1.7.30.jar", "log4j-1.2.17.jar", "log4j-api-2.17.1.jar"
+	private static final Logger logger = LoggerFactory.getLogger(ComplexSort.class);
 	private static List<Product> products = new ArrayList<>();
 
 	public static void main(String[] args) {
@@ -22,6 +26,8 @@ public class ComplexSort {
 		
 		Collections.sort(products, chain);
 		
+		consoleLog();
+		
 	}
 	
 	/**
@@ -34,7 +40,17 @@ public class ComplexSort {
 	 * 상품 사이즈를 정렬
 	 */
 	public static Comparator<Product> comparatorPrdSize = Comparator.comparing(Product::getPrdSize);
-			
+		
+	/**
+	 * 대상 객체의 출력
+	 */
+	private static void consoleLog() {
+		logger.info("######## 출력 시작 ########");
+		for (Product product : products) {
+			logger.info("Print : {} = {}", product.getPrdId(), product.getPrdSize());
+		}
+	}
+	
 	/**
 	 * 미리 데이터를 생성
 	 */

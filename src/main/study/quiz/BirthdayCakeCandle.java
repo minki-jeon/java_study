b520c7006dd9f18a514869d616510528049b7771
minki-jeon <minki.jeon.study@gmail.com>
2025-03-05 20:03:29 +0900

    프로그램에 대한 주석 추가

--- src/main/study/quiz/BirthdayCakeCandle.java
+++ src/main/study/quiz/BirthdayCakeCandle.java
@@ -18,6 +18,7 @@ public class BirthdayCakeCandle {
 		for (int ar_i = 0; ar_i < n; ar_i++) {
 			ar[ar_i] = in.nextInt();
 		}
+		//* 입력받은 값 중에 중복된 숫자를 카운트하고 최대 카운트를 result 반환
 		int result = birthdayCakeCandles(n, ar);
 		System.out.println(result);
 	}
@@ -27,7 +28,7 @@ public class BirthdayCakeCandle {
 		
 		// 목록을 돌면서 key가 존재할 경우 +1, 그렇지 않으면 1을 value에 저장
 		for (int i = 0; i < ar.length; i++) {
-			if4 (map.containsKey(ar[i])) {	//* key 존재 여부 확인
+			if (map.containsKey(ar[i])) {	//* key 존재 여부 확인
 				map.put(ar[i], map.get(ar[i]) + 1);
 			} else {
 				map.put(ar[i], 1);
